a9ae8df307c78e47f148aaf641e2e38dc9ab3564
MilanMareels <milan.mareels@gmail.com>
2025-06-23 20:40:49 +0200

    url change

--- src/api.ts
+++ src/api.ts
@@ -1,4 +1,4 @@
-const BASE_URL = "http://localhost:3000/api/v1";
+const BASE_URL = "https://clocky-backend.vercel.app/api/v1";
 
 async function postJSON(path: string, data: any) {
   const res = await fetch(BASE_URL + path, {
